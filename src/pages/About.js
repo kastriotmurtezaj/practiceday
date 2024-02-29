0a2e1351f993b6a2bc5b8a2086355684dcdda35a
kastriotmurtezaj <murtezajkastriot@gmail.com>
2024-02-29 23:35:24 +0100

    praktikimi

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import App from '../App';
+import React from 'react';
+
+export default function About() {
+    return (
+        <div>
+            <App/>
+            <h2>About Page</h2>
+        </div>
+    )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import App from '../App';
+import React from 'react';
+
+
+export default function Home() {
+    return (
+        <div>
+            <App/>
+            <h2>Home Page</h2>
+        </div>
+    )
+}
